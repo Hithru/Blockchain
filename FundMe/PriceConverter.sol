4158494a61a5ab9a175918af15a0b3ca62211c4a
Hithru <hithrudealwis@gmail.com>
2022-10-13 15:29:35 +0530

    Tutorial: Fund Me

---
+++ FundMe/PriceConverter.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    
+    function getPrice() internal view returns(uint256){
+        //ABI
+        //Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int price,,,) = priceFeed.latestRoundData();
+        // Ehth in terms of USD decimals 8
+        // 3000.00000000
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
